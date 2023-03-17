ec86887142b4ccc315bf0933857189c5650d958f
kristiinms <114983014+kristiinms@users.noreply.github.com>
2023-03-17 14:39:37 +0200

    Add files via upload
    Tehtud

--- 05_kontrolltoo1/KeskmineKiirus2.java
+++ 05_kontrolltoo1/KeskmineKiirus2.java
@@ -1,23 +1,28 @@
 public class KeskmineKiirus2 {
 
-    //funktsioonile antakse 1km-de lõikude läbimise kiirused. väljasta kogu tee keskmise kiirus.
-        public static void main(String[] args) {
-            double[] kiirused = {60.0, 50.0, 70.0}; // kiirused kilomeetrites tunnis
-            double[] vahemaa = {1.0, 1.0, 1.0}; // lõikude pikkused kilomeetrites
-            double totalDistance = 0.0;
-            double weightedSpeedSum = 0.0;
-
-            // leiame kogu tee pikkuse
-            for (double distance : vahemaa) {
-                totalDistance += distance;
-            }
-
-            // leiame keskmise kiiruse
-            for (int i = 0; i < kiirused.length; i++) {
-                weightedSpeedSum += kiirused[i] * vahemaa[i];
-            }
-            double averageSpeed = weightedSpeedSum / totalDistance;
-
-            System.out.println("Kogu tee läbimise keskmine kiirus: " + averageSpeed + " km/h");
-        }
+    // funktsioonile antakse 1km-de lõikude läbimise kiirused. väljasta kogu tee keskmise kiirus.
+    public static void main(String[] args) {
+        double[] kiirused = {60.0, 50.0, 70.0}; // kiirused kilomeetrites tunnis
+        double[] vahemaa = {1.0, 1.0, 1.0}; // lõikude pikkused kilomeetrites
+        Kiirus2(kiirused, vahemaa);
     }
+
+    private static void Kiirus2(double[] kiirused, double[] vahemaa) {
+        double koguVahemaa = 0.0;
+        double koguKiirus = 0.0;
+
+        // kogu tee pikkus
+        for (double vahemaad : vahemaa) {
+            koguVahemaa += vahemaad;
+        }
+
+        // kesk. kiirus
+        for (int i = 0; i < kiirused.length; i++) {
+            koguKiirus += kiirused[i] * vahemaa[i];
+        }
+
+        double keskmine = koguKiirus / koguVahemaa;
+
+        System.out.println("Kogu tee läbimise keskmine kiirus: " + keskmine + " km/h");
+    }
+}
